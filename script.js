8f4bab35bf11d61f2fd045f8b4840444b2252884
devibifaa <ib2link@yahoo.com>
2023-11-29 04:24:11 +0100

    completed all functionality

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ class Calculator{
         this.clear();
     }
 
-    delete(){
-            this.currentOperand = this.currentOperand.slice(0,-1);
-            // console.log("I am deleted")
-        }
-
     clear(){
         this.prevOperand = '';
         this.currentOperand ='';
@@ -19,12 +14,16 @@ class Calculator{
 
     }
 
+    delete(){
+            this.currentOperand = this.currentOperand.slice(0,-1);
+        }
+
+    
+
     appendNumber(number){
        
         if(number== '.'  && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString();
-        // console.log("i am a number")
-
     }
 
     selectOperation(operation){
@@ -70,39 +69,120 @@ class Calculator{
             this.operation = undefined;
             this.prevOperand = '';
 
+            console.log(computation)
+
     }
 
-    getDisplayNumber(number){
-        let stringNumber = number.toString()
-        let integerDigits = parseFloat(stringNumber.split('.')[0])
-        let decimalDigits = stringNumber.split('.')[1];
-      let integerDisplay;
-       if(isNaN(integerDigits)){
-        integerDigits = ''
-       } else {
-        integerDisplay = integerDigits.toLocaleString('en', {
-                minimumFractionDigits:0})
-       }
-       if(decimalDigits != null){
-        return `${integerDisplay}.${decimalDigits}`
-       } else{
-        return integerDisplay;
-       }
-    }
-
-
-
-    updateDisplay(){
-        this.currentOperandText.innerText = 
-        this.getDisplayNumber(this.currentOperand);
-        if(this.operation != null){
-            this.prevOperandText.innerText = 
-            `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
-        } else {
-            this.currentOperandText.innerText=''
-        }
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const [integerPart, decimalPart] = stringNumber.split('.');
         
-    }
+        const formattedInteger = isNaN(parseInt(integerPart))
+      ? ''
+      : parseInt(integerPart).toLocaleString('en', { minimumFractionDigits: 0 });
+    
+        if (decimalPart) {
+          return `${formattedInteger}.${decimalPart}`;
+        } else {
+          return formattedInteger;
+        }
+      }
+    
+      updateDisplay() {
+        this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
+    
+        if (this.operation !== undefined) {
+          this.prevOperandText.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
+        } else {
+          this.prevOperandText.innerText = '';
+        }
+      }
+
+//     getDisplayNumber(number){
+//         let stringNumber = number.toString()
+//         let integerDigits = parseInt(stringNumber.split('.')[0])
+//         let decimalDigits = stringNumber.split('.')[1];
+     
+//       if(isNaN(integerDigits)){
+//         return  '' 
+//       } else{
+//         let integerDisplay = integerDigits.toLocaleString('en', {minimumFractionDigits:0})
+//         return decimalDigits ? console.log(`InterDisplay: ${integerDisplay}.${decimalDigits}`) : console.log(`InterDisplay: ${integerDisplay}`);
+//       }
+//     //  const formattedInteger = isNaN(number) ? '' : integerDisplay;
+//     // return decimalDigits ? `${integerDisplay}.${decimalDigits}` : integerDisplay;
+
+//     }
+
+    
+ 
+
+// // if(isNaN(integerDigits)) {
+// //     integerDigits == ''
+// // } else {
+    
+// // }if(decimalDigits != null){
+// //     return integerDisplay;
+// // }
+
+      
+//     //    if(isNaN(integerDigits)){
+//     //     integerDigits = ''
+//     //    } else {
+//     //     integerDisplay = integerDigits.toLocaleString('en', {
+//     //             minimumFractionDigits:0})
+//     //    }
+//     //    if(decimalDigits != null){
+//     //     return `${integerDisplay}.${decimalDigits}`
+//     //    } else{
+//     //     return integerDisplay;
+//     //    }
+
+//     //    console.log(integerDisplay)
+    
+
+//     // getDisplayNumber(number) {
+//     //     const formattedInteger = isNaN(number) ? '' : number.toLocaleString('en', { minimumFractionDigits: 0 });
+//     //     const decimalDigits = number.toString().split('.')[1];
+        
+    
+//     //     return decimalDigits ? `${formattedInteger}.${decimalDigits}` : formattedInteger;
+
+   
+
+        
+//     // }
+    
+
+
+
+//     updateDisplay(){
+
+//         this.currentOperandText.innerText = this.getDisplayNumber(this.currentOperand)
+//         console.log(`currentOperandText: ${currentOperandText}`)
+
+//         if(this.operation !== null){
+//             if (this.prevOperand !== null) {
+//                 this.prevOperandText.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`;
+//         } else{
+//             currentOperandText.innerText = this.getDisplayNumber(this.currentOperand);
+  
+//         }
+//     }
+// }
+        
+          // this.currentOperandText.innerText = this.currentOperand;
+        // this.prevOperandText.innerText = this.prevOperand}
+    //     let checkedValue = this.getDisplayNumber(this.currentOperand);
+    //     console.log(checkedValue)
+    //     let returnValue = checkedValue.toString() + this.operation.toString()
+    //     if(this.operation != null){
+    //     return this.currentOperandText.innerText = returnValue
+    //     } else {
+    //         this.currentOperandText.innerText=''
+    //     }
+        
+    // }
 
 }
 
@@ -125,7 +205,7 @@ let clearButton = document.querySelector("[data-all-clear]")
 const calculator = new Calculator(prevOperandText, currentOperandText);
 numberBtn.forEach(button => {
     button.addEventListener("click", ()=>{
-        calculator.appendNumber(button.innerHTML);
+        calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
 
         
@@ -136,7 +216,7 @@ numberBtn.forEach(button => {
 
     operatorsBtn.forEach(button => {
         button.addEventListener("click", ()=>{
-            calculator.selectOperation(button.innerHTML);
+            calculator.selectOperation(button.innerText);
             calculator.updateDisplay();
         })
         
